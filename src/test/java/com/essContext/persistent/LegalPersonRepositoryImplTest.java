e9b206b213cf063bd238faa38bfc9f6c378f4f1d
hanlijun <hanlijun>
2020-07-25 15:34:03 +0800

    +

--- src/test/java/com/essContext/persistent/LegalPersonRepositoryImplTest.java
+++ src/test/java/com/essContext/persistent/LegalPersonRepositoryImplTest.java
@@ -9,26 +9,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class LegalPersonRepositoryImplTest extends BaseTest {
-
+public class LegalPersonRepositoryImplTest extends BaseTest {
     @Autowired
     LegalPersonService legalPersonService;
 
     @Test
     void should_return_services_success_when_register_given_legalPerson_info() throws Exception {
-
         LegalPersonRequest request = new LegalPersonRequest();
-
         request.setType("1");
         request.setCompanyCode("99999999999");
         request.setCompanyName("中国渔场");
         request.setName("中国渔场");
         request.setIdCode("2222222222222");
         request.setIdType("1");
-
         LegalPerson legalPerson = legalPersonService.register(request);
-
+        System.out.println(legalPerson);
         assertTrue(legalPerson.getId().length()>0);
-        
     }
 }
